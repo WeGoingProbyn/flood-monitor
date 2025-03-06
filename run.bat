c685c88743578badba9ed00e0fffc4a0e47d1b90
WeGoingProbyn <williamprobyn123@gmail.com>
2025-03-06 16:55:05 +0000

    Add minimum python version requirement to run scripts for both sh and bat

--- run.bat
+++ run.bat
@@ -1,8 +1,19 @@
 @echo off
+
+REM Check if Python version is at least 3.7
+python -c "import sys; assert sys.version_info >= (3,10), 'Python 3.10 or higher is required'" 
+if %errorlevel% neq 0 (
+  echo ERROR: Python 3.10 or higher is required.
+  pause
+  exit /b 1
+) else (
+  echo Python minimum version met...
+)
+
 REM Check if the virtual environment exists by looking for the activation script
 if not exist ".venv\Scripts\activate.bat" (
-    echo Creating virtual environment...
-    python -m venv .venv
+  echo Creating virtual environment...
+  python -m venv .venv
 )
 
 REM Activate the virtual environment
--- run.sh
+++ run.sh
@@ -1,5 +1,17 @@
 #!/bin/bash
 
+# Minimum required Python version
+REQUIRED_PYTHON_VERSION="3.10"
+
+# Check if the installed Python version meets the minimum requirement.
+python3 -c "import sys; assert sys.version_info >= (3,10), 'Python 3.10 or higher is required'" 2>/dev/null
+if [ $? -ne 0 ]; then
+  echo "ERROR: Python $REQUIRED_PYTHON_VERSION or higher is required."
+  exit 1
+else
+  echo "Minimum python version met..."
+fi
+
 # Check if a virtual environment directory (venv) exists; if not, create one
 if [ ! -d ".venv" ]; then
   echo "Creating virtual environment..."
